512071354943e3e92a5288d8d26dbfabb1abcd65
MasterPpv <pritenvora@gmail.com>
2014-11-14 14:02:57 -0600

    Got it working to where the user is currently asked for something to search for, and then all incoming tweets are filtered using that search query and added into a local MongoDB database.

--- Analyst/src/analyst.go
+++ Analyst/src/analyst.go
@@ -12,6 +12,8 @@ import (
 	"github.com/garyburd/twitterstream"
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/nsf/termbox-go"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var (
@@ -55,6 +57,7 @@ func termboxQuery() string {
 	termbox.SetInputMode(termbox.InputEsc)
 	edit_box.InsertRune('#')
 
+	// Queries the user for a hastag to search
 	redraw_all()
 	query := ""
 	stringSlices := make([]rune, 0)
@@ -64,14 +67,18 @@ func termboxQuery() string {
 		case termbox.EventKey:
 			switch ev.Key {
 			case termbox.KeyEsc:
+				// If Esc is pressed, abort query
 				break queryloop
 			case termbox.KeySpace:
+				// If space is pressed, add a space
 				edit_box.InsertRune(' ')
 				stringSlices = append(stringSlices, ' ')
 			case termbox.KeyEnter:
+				// If Enter pressed, use query 
 				query = string(stringSlices)
 				break queryloop
 			case termbox.KeyBackspace, termbox.KeyBackspace2:
+				// If Backspace is pressed, delete a letter
 				edit_box.DeleteRuneBackward()
 				if len(stringSlices) > 0 {
 					tempSlices := make([]rune, len(stringSlices) - 1)
@@ -80,6 +87,8 @@ func termboxQuery() string {
 					stringSlices = tempSlices
 				}
 			default:
+				// If any other key is pressed, add the
+				// corresponding letter to the query string
 				if ev.Ch != 0 {
 					edit_box.InsertRune(ev.Ch)
 					stringSlices = append(stringSlices, ev.Ch)
@@ -92,47 +101,70 @@ func termboxQuery() string {
 	}
 	if query == "" || query == "#" || query == " " || query == "# " {
 		fmt.Println("No query given. Nothing to search for. Program exiting...")
+		return query
 	} else {
 		termbox.Close()
 		fmt.Print("Searching for: ", query, "\n")
 		fmt.Println()
+		return query
 	}
-	return query
 }
 
 func main() {
+	// Query the user for a search entry
 	query := termboxQuery()
+	// Deal with the empty cases
 	if query == "" || query == "#" || query == " " || query == "# " {
 		return
 	}
 
+	// Read OAuth credentials from config file
 	flag.Usage = usage
 	flag.Parse()
 	if err := readConfig(); err != nil {
 		log.Fatalf("Error reading configuration, %v", err)
 	}
 
-	ts, err := twitterstream.Open(
+	mdbSession, mdbErr := mgo.Dial("127.0.0.1:27017")
+	if mdbErr != nil {
+		panic(mdbErr)
+	}
+	defer mdbSession.Close()
+
+	// Open a Twitter stream using OAuth credentials and start
+	// listening for Tweets containing the user's query
+	ts, tsErr := twitterstream.Open(
 		&oauthClient,
 		&accessToken,
 		"https://stream.twitter.com/1.1/statuses/filter.json",
 		url.Values{"track": {query}})
-	if err != nil {
-		log.Fatal(err)
+	if tsErr != nil {
+		log.Fatal(tsErr)
 	}
 	defer ts.Close()
 
+	tweetCollection := mdbSession.DB("test").C("Tweets")
 	// Loop until stream has a permanent error.
 	for ts.Err() == nil {
+		// As each tweet comes in, mine it for data and print it
 		var t anaconda.Tweet
-		if err := ts.UnmarshalNext(&t); err != nil {
-			log.Fatal(err)
+		if tweetErr := ts.UnmarshalNext(&t); tweetErr != nil {
+			log.Fatal(tweetErr)
 		}
 		fmt.Print("Username: @", t.User.ScreenName, "\n")
 		fmt.Print("Tweet: ", t.Text, "\n")
 		fmt.Print("URL: https://twitter.com/", t.User.ScreenName, "/status/", t.Id)
 		fmt.Println()
 		fmt.Println()
+		tweetM := bson.M {
+			"ScreenName":t.User.ScreenName,
+			"Text":t.Text,
+			"Id":t.Id,
+		}
+		insertErr := tweetCollection.Insert(&tweetM)
+		if insertErr != nil {
+			log.Fatal(insertErr)
+		}
 	}
 	log.Print(ts.Err)
 }
